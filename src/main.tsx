2bc576dc59c632a2cb7ba4c17c3fce0e200140a4
Arshavir Azarfar <4rsh4vir4z4rf4r@gmail.com>
2024-01-02 08:35:09 -0800

    mantine added

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { createTheme, MantineProvider } from '@mantine/core';
 
-import "./styles/global.css";
-import { router } from "./router.tsx";
-import { GlobalProvider } from "./providers";
+import './styles/global.css';
+import { router } from './router.tsx';
+import { GlobalProvider } from './providers';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const theme = createTheme({});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <GlobalProvider>
-    <RouterProvider router={router} />
+    <MantineProvider theme={theme}>
+      <RouterProvider router={router} />
+    </MantineProvider>
   </GlobalProvider>
 );
